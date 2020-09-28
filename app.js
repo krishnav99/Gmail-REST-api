c50f280735ce84d770a0c910d5ac1d09f2b4616d
Krishnava Mahanta <krishnavmahanta1999@gmail.com>
2020-09-29 00:24:54 +0530

    Added nodemailer
    Using nodemailer to send email to the each of the client email adresses stored in the file.

--- app.js
+++ app.js
@@ -3,13 +3,15 @@ var express         = require("express"),
     bodyParser      = require("body-parser"),
     passport        = require("passport"),
     GoogleStrategy  = require('passport-google-oauth20'),
+    nodemailer      = require("nodemailer"),
     Credentials     = require("./credentials"), //requiring credentials from a private file
+    User            = require("./nodemaileruser"), //getting user data for nodemailer
     fs              = require("fs");
+const { get } = require("http");
 
 var app = express();
 var port= process.env.PORT || 3000;
-
-
+var emailList = [];
 app.use(bodyParser.urlencoded({extended:true}));
 
 //Google Authentication Strategy for passport
@@ -41,6 +43,15 @@ passport.deserializeUser(function(user, done) {
     done(null, user);  
 });
 
+//setting up nodemailer transporter
+var transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: User.mailId,
+    pass: User.password,
+  }
+});
+
 //Index route 
 app.get("/", function(req, res){
     res.send("Home Page");
@@ -60,9 +71,8 @@ app.get('/google/callback', passport.authenticate('google', { failureRedirect: '
 function storeData(data){
   if(fs.existsSync('data.json')){//checking if the file exists;
     fs.readFile('data.json', function(err, readdata){  //reading data from the file
-        if(err) throw err;
-        console.log(JSON.parse(readdata));
-        if(!readdata.includes(JSON.stringify(data))){   //checking if the data does not exists previously
+        if(err) throw err; 
+        if(!readdata.includes(JSON.stringify(data))){   //checking if the data does not exist previously
           fs.writeFile("data.json", JSON.stringify(JSON.parse(readdata).concat(data)), function (err) {   //writing the data onto the file
             if (err) throw err;
             console.log('Saved!')}); 
@@ -70,11 +80,41 @@ function storeData(data){
       });
     }
   else{
-    fs.writeFile("data.json", JSON.stringify([data]), function (err) {  //if the file does not exist we are creating the file and writing the data
+    fs.writeFile("data.json", JSON.stringify([data]), function (err) {  //if the file does not exist we create a new file and save the user data
       if (err) throw err;
       console.log('Saved!')});
   }   
 };
+
+//Route to send mail
+app.get("/sendmail", function(req, res){
+  fs.readFile('data.json', function(err, readdata){  //read the client data stored in the file 
+    if(err){
+      console.log(err);
+      res.redirect("/")
+    }
+    for (const element of JSON.parse(readdata)){      //loops through each client data and stores the email in an array
+      if(!emailList.includes(element.emails[0].value)){
+        emailList.push(element.emails[0].value)
+      }
+    }
+    console.log(JSON.parse(readdata))
+    transporter.sendMail(mailOptions = {        //sends an email to each email adress in the email array
+      from: User.mailId,
+      subject: 'Sending Email from Node.js',
+      text: 'Test Message',
+      to:emailList  
+      }, function(error, info){ 
+        if (error) {
+          console.log(error);
+          res.redirect("/")
+        } else {
+          console.log('Email sent: ' + info.response);
+          res.send("Email Sent")
+        }
+    });
+  });
+});
 
 
 //Logout route
